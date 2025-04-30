ed7c13987731e63ecb75c7fdcc3385f365c9e865
luismues <luis.mueschen@hs-augsburg.de>
2025-04-30 14:54:30 +0200

    Switched client registering from dictionary to built in groups

--- signalRServer/MyHub.cs
+++ signalRServer/MyHub.cs
@@ -3,11 +3,10 @@ using Microsoft.AspNetCore.SignalR;
 
 public class MyHub : Hub
 {
-    public static Dictionary<string, string> ConnectedClients = new();
-    public Task Register(string clientID){
+    public Task Register(string groupName){
 
-        ConnectedClients[clientID] = Context.ConnectionId;
-        Console.WriteLine($"Client verbunden: {clientID}");
+        Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+        Console.WriteLine($"Client {Context.ConnectionId} mit Gruppe {groupName} verbunden");
         return Task.CompletedTask;
     }
     public async Task Ping(){
@@ -16,24 +15,24 @@ public class MyHub : Hub
     public async Task SendToBackend(string payload)
     {
         Console.WriteLine($"Vom Frontend empfangen: {payload}");
-        await Clients.Client(ConnectedClients["backend"]).SendAsync("CheckConnection", payload);
+        await Clients.Group("backend").SendAsync("CheckConnection", payload);
     }
 
     public async Task SendToFrontend(string payload)
     {
         Console.WriteLine($"Vom Backend empfangen: {payload}");
-        await Clients.Client(ConnectedClients["frontend"]).SendAsync("ReceiveMessage", payload);
+        await Clients.Group("frontend").SendAsync("ReceiveMessage", payload);
     }
 
     public async Task NotifyBackendAboutFileUpload(string filename)
     {
         Console.WriteLine("Datei wurde hochgeladen: " + filename);
-        await Clients.Client(ConnectedClients["backend"]).SendAsync("FileUploaded", filename);
+        await Clients.Group("backend").SendAsync("FileUploaded", filename);
     }
 
     public async Task NotifyFrontendAboutManipulatedMesh(string filename)
     {
         Console.WriteLine("Mesh wurde bearbeitet: " + filename);
-        await Clients.Client(ConnectedClients["frontend"]).SendAsync("MeshTransformed", filename);
+        await Clients.Group("frontend").SendAsync("MeshTransformed", filename);
     }
 }
