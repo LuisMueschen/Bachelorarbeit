7c2287058042e742bb43e0f845ee12b4ad08fffe
luismues <luis.mueschen@hs-augsburg.de>
2025-06-06 11:28:49 +0200

    More detailed commenting of relief script

--- worker/heightmap.py
+++ worker/heightmap.py
@@ -4,35 +4,39 @@ import trimesh
 
 def create_relief(image_path, output_path, scale_x=1.0, scale_y=1.0, scale_z=1.0, invert=False):
     # Bild laden und in Graustufen umwandeln
-    print(image_path)
-    print(output_path)
     img = Image.open(image_path).convert('L')
+
+    # heightmap als 2-Dim Array erstellen
     heightmap = np.array(img, dtype=np.float32)
 
     if invert:
         heightmap = 255 - heightmap
 
-    heightmap *= scale_z / 255.0  # Höhenwerte normalisieren
+    # heightmap werte auf werte zwischen 0 und scale_z normalisieren
+    heightmap *= scale_z / 255.0
 
+    # Höhe und Breite von heigtmap
     h, w = heightmap.shape
     vertices = []
     faces = []
 
-    # Vertices generieren
+    # Über Pixel in Bild / Koordinaten in Heightmap itterieren und Koordinaten mit Höhenwert als Vertices erstellen
     for y in range(h):
         for x in range(w):
+            # z = Höhenwert an Koordinate x,y
             z = heightmap[y, x]
             vertices.append((x * scale_x, y * scale_y, z))
 
-    # Faces erzeugen
+    # Funktion um 2-Dim Arrayindex zu 1-Dim Index umzuwandeln
     def idx(x, y): return y * w + x
 
+    # Über Pixel in Bild / Koordinaten in Heightmap itterieren und mit idx funktion faces erstellen
     for y in range(h - 1):
         for x in range(w - 1):
-            # Zwei Dreiecke pro Quadrat
+            # Zwei Dreiecke pro Quadrat - um Koordinaten nicht doppelt zu speichern werden im faces array nur Indizes des vertices array gespeichert
             faces.append([idx(x, y), idx(x+1, y), idx(x, y+1)])
             faces.append([idx(x+1, y), idx(x+1, y+1), idx(x, y+1)])
     
-    print("done")
     mesh = trimesh.Trimesh(vertices=vertices, faces=faces)
     mesh.export(output_path)
+    print("Relief erstellt")
